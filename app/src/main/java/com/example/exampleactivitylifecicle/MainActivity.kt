22a42948fcbeaa1b78b24713284102ac704af306
goslip05 <djimenez750@uan.edu.co>
2023-05-02 22:10:02 -0500

    update

--- app/src/main/java/com/example/exampleactivitylifecicle/MainActivity.kt
+++ app/src/main/java/com/example/exampleactivitylifecicle/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.exampleactivitylifecicle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        println("this in the onCreated")
+        Log.d("TAGLIFECYVLE","THIS IS THE ONCREATED")
     }
 }
