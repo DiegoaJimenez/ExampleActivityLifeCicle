bff40307a4b0dc9ec2d34a6ddbed00d273b9d005
goslip05 <djimenez750@uan.edu.co>
2023-05-02 23:19:20 -0500

    update

--- app/src/main/java/com/example/exampleactivitylifecicle/MainActivity.kt
+++ app/src/main/java/com/example/exampleactivitylifecicle/MainActivity.kt
@@ -3,12 +3,46 @@ package com.example.exampleactivitylifecicle
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         println("this in the onCreated")
+        val b = findViewById<Button>(R.id.button)
+        b.setOnClickListener { showImage() }
         Log.d("TAGLIFECYVLE","THIS IS THE ONCREATED")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("TAGLIFECYVLE","THIS IS THE ONSTART")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("TAGLIFECYVLE","THIS IS THE ONSTOP")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("TAGLIFECYVLE","THIS IS THE ONPAUSE")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("TAGLIFECYVLE","THIS IS THE ONRESUME")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("TAGLIFECYVLE","THIS IS THE ONDESTROY")
+    }
+
+    fun showImage(){
+        val iv = findViewById<ImageView>(R.id.imageView)
+        iv.setImageResource(R.drawable.bayern)
     }
 }
